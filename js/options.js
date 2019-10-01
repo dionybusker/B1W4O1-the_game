aa9968c97c0f10c2932d23df1cc535b139931eaa
dionybusker <99057959@mydavinci.nl>
2019-10-01 09:27:21 +0200

    Nieuwe repository
    Alle bestanden uit eerdere mappen worden in een nieuwe repo geplaatst op Github.

---
+++ js/options.js
@@ -0,0 +1,27 @@
+var event1option1 = "Optie 1: je blijft zitten en wacht het uit."; // option1 op event1
+var event1option2 = "Optie 2: je staat op en gaat op onderzoek uit."; // option2 op event1
+
+var event3option1 = "Optie 1: je gaat naar links"; // option1 op event3
+var event3option2 = "Optie 2: je gaat naar rechts"; // option2 op event3
+
+var event4option1 = "Optie 1: je opent de deur en loopt de ruimte binnen"; // option1 op event4
+var event4option2 = "Optie 2: je loopt terug naar de andere kant van de gang"; // option2 op event 4
+
+var event6option1 = "Optie 1: je opent de lade en bekijkt de mappen"; // option1 op event6
+var event6option2 = "Optie 2: je gaat verder door de volgende deur"; // option2 op event6
+
+var event9option1 = "Optie 1: je besluit de map te bekijken"; // option1 op event9
+var event9option2 = "Optie 2: je laat de map liggen en loopt toch naar de deur"; // option2 op event9
+
+var event11wait = "Hoeveel minuten wil je wachten?"; // er wordt gevraagd hoelang de speler wilt wachten
+// var event11option1 = "Optie 1: je blijft staan en wacht het af"; // option1 op event11 // hoelang wacht je het af? // als je 5 minuten wacht en je je daarna verstopt zit je goed, als je 10 min wacht wordt je gepakt
+// var event11option2 = "Optie 2: je verstopt je toch in de kast"; // option2 op event11
+
+var event13option1 = "Optie 1: je zet je telefoon uit"; // option1 op event13
+var event13option2 = "Optie 2: je neemt op"; // option2 op event13
+
+var event14option1 = "Optie 1: je kruipt door de kruipruimte"; // option1 op event14
+var event14option2 = "Optie 2: je vertrouwt het niet en je besluit in de huidige ruimte te blijven"; // option2 op event14
+
+var event16option1 = "Optie 1: je staat op en pakt de mondkap van de kast"; // option1 op event16
+var event16option2 = "Optie 2: je blijft lekker zitten en knijpt je neus maar dicht"; // option2 op event16
